a9fe5cb6866db9e0506cef769a267ce73fe0c495
VictorStehnei <victor.stehnei@internet-techonlogy-hub.com>
2025-03-17 14:04:47 +0200

    Initialize new project with layered architecture

---
+++ DeadWallet/DeadWallet.DAL/Data/DeadWalletContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeadWallet.DAL
+{
+    public class DeadWalletContext : DbContext
+    {
+        public DeadWalletContext (DbContextOptions<DeadWalletContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ DeadWallet/DeadWallet.PL/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DeadWallet.PL.Models;
+
+namespace DeadWaller.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        // Example using of logger
+        _logger.LogInformation("This is an information log message.");
+        _logger.LogWarning("This is a warning log message.");
+        _logger.LogError("This is an error log message.");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
