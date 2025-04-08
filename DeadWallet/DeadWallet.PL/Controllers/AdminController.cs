a08d03f83c667aa7b57785597e78e2303f1c7f9d
denys <denys@DESKTOP-89JAFHM>
2025-04-08 12:05:22 +0300

    Додав вкладку Tags.
    В контролері створив метод для отримання тегів.
    
    Додав вивід списку тегів у відповідному табі.
    
    Додав кнопку для видалення тегу.

--- DeadWallet/DeadWallet.PL/Controllers/AdminController.cs
+++ DeadWallet/DeadWallet.PL/Controllers/AdminController.cs
@@ -5,10 +5,32 @@ namespace DeadWallet.PL.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ITagService _tagService;
+
+        public AdminController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+        [Authorize(Policy = "AdminOnly")]
+        public async Task<IActionResult> Tags()
+        {
+            var tags = await _tagService.GetAllTagsAsync();
+            return PartialView("_TagsPartial", tags);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "AdminOnly")]
+        public async Task<IActionResult> DeleteTag(int id)
+        {
+            await _tagService.DeleteTagAsync(id);
+            return RedirectToAction("Tags");
+        }
+
         [Authorize(Policy = "AdminOnly")]
         public IActionResult Index()
         {
             return View();
         }
     }
+
 }
