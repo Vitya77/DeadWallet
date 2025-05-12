ea353421a137781deaae70b5a6c81dac3cc6ce79
denys <denys@DESKTOP-89JAFHM>
2025-05-12 17:40:39 +0300

    transaction admin panel

--- DeadWallet/DeadWallet.PL/Controllers/AdminController.cs
+++ DeadWallet/DeadWallet.PL/Controllers/AdminController.cs
@@ -13,19 +13,34 @@ namespace DeadWallet.PL.Controllers
     {
         private readonly ITagService _tagService;
         private readonly UserService _userService;
+        private readonly ITransactionService _transactionService;
 
-        public AdminController(ITagService tagService, UserService userService)
+        public AdminController(ITagService tagService, UserService userService, ITransactionService transactionService)
         {
             _tagService = tagService;
             _userService = userService;
+            _transactionService = transactionService;
+        }
+
+        public async Task<IActionResult> Transactions()
+        {
+            var transactions = await _transactionService.GetAllTransactionsAsync();
+            return PartialView("_TransactionsPartial", transactions);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteTransaction(int id)
+        {
+            await _transactionService.DeleteTransactionAsync(id);
+            return RedirectToAction("Index");
         }
 
         public async Task<IActionResult> Index()
         {
+            var transactions = await _transactionService.GetAllTransactionsAsync();
             var tags = await _tagService.GetAllTagsAsync();
             var users = await _userService.GetAllUsersAsync();
 
-            return View(new AdminViewModel { tags = tags, users = users.Res });
+            return View(new AdminViewModel { tags = tags, users = users.Res, transactions = transactions.Res });
         }
 
         public async Task<IActionResult> Tags()
--- DeadWallet/DeadWallet.PL/Models/AdminViewModel.cs
+++ DeadWallet/DeadWallet.PL/Models/AdminViewModel.cs
@@ -2,8 +2,11 @@
 
 namespace DeadWallet.PL.Models
 {
+
     public class AdminViewModel
     {
+        public IEnumerable<Transaction> transactions { get; set; }
+
         public IEnumerable<Tag> tags { get; set; }
         public IEnumerable<DeadWalletUser> users { get; set; }
     }
